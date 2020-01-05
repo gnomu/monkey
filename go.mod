088ea8a105c3c6342a034cad64695c0587b40c29
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2020-01-06 00:05:21 +0900

    Added lexer test

---
+++ go.mod
@@ -0,0 +1,5 @@
+module monkey
+
+go 1.13
+
+require github.com/jun06t/monkey v0.0.0-20200105145311-bf15bccf6b34
---
+++ lexer/lexer_test.go
@@ -0,0 +1,37 @@
+package lexer
+
+import (
+	"testing"
+
+	"github.com/jun06t/monkey/token"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=_(){},;`
+
+	tests := []struct {
+		expectType    token.TokenType
+		expectLiteral string
+	}{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectType {
+			t.Fatalf("tests[%d] - tokentype wrong.expected=%q, got=%q", i, tt.expectType, tok.Type)
+		}
+		if tok.Literal != tt.expectLiteral {
+			t.Fatalf("tests[%d] - literal wrong.expected=%q, got=%q", i, tt.expectLiteral, tok.Literal)
+		}
+	}
+}
