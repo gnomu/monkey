bf15bccf6b34ed88987fa56e45eda4611f620f93
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2020-01-05 23:53:11 +0900

    Added token type

--- token/token.go
+++ token/token.go
@@ -6,3 +6,28 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// 識別子, リテラル
+	IDENT = "IDNET"
+	INT   = "INT"
+
+	// 演算子
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// デリミタ
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	//キーワード
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
